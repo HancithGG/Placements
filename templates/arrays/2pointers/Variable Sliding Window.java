4a49c6a5694cdd8c5d29b55bcaf3dcbf0691f40e
HancithGG <learneasily.2534@gmail.com>
2026-06-05 11:48:22 +0530

    Update Variable Sliding Window.java

--- templates/arrays/2pointers/Variable Sliding Window.java
+++ templates/arrays/2pointers/Variable Sliding Window.java
@@ -1,14 +1,24 @@
 int left = 0;
+int windowValue = 0;
+
+int answer = 0;
 
 for(int right = 0; right < arr.length; right++){
 
-    // expand
+    // Element entering window
+    windowValue += arr[right];
 
+    // Window invalid?
     while(condition breaks){
 
-        // shrink
+        // Element leaving window
+        windowValue -= arr[left];
         left++;
     }
 
-    // update answer
+    // Current valid window
+    answer = Math.max(answer,
+                      right - left + 1);
 }
+
+System.out.println(answer);

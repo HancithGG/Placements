64ad0c548c31cf8e9d452ad96c6d21df49a23a9e
HancithGG <learneasily.2534@gmail.com>
2026-06-05 18:14:40 +0530

    Refactor partition method with two-pointer approach
    Refactor partition logic to use two-pointer technique for better efficiency.

--- templates/arrays/2pointers/partition.java
+++ templates/arrays/2pointers/partition.java
@@ -1,40 +1,13 @@
-//basic one 
+int slow = 0;
 
-int left = 0;
+for(int fast = 0; fast < arr.length; fast++){
 
-for(int right = 0; right < arr.length; right++) {
+    if(condition){
 
-    if(arr[right] % 2 == 0) {
+        int temp = arr[fast];
+        arr[fast] = arr[slow];
+        arr[slow] = temp;
 
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-
-        left++;
+        slow++;
     }
 }
-
-
-// advanced one
-
-int pivot = arr[high];
-
-int i = low;
-
-for(int j = low; j < high; j++) {
-
-    if(arr[j] <= pivot) {
-
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-
-        i++;
-    }
-}
-
-int temp = arr[i];
-arr[i] = arr[high];
-arr[high] = temp;
-
-return i;

2478db35a3401a6eb9e355c8e8bb8a92157a1bc9
HancithGG <learneasily.2534@gmail.com>
2026-06-05 11:41:06 +0530

    Create Variable Sliding Window.java

---
+++ templates/arrays/2pointers/Variable Sliding Window.java
@@ -0,0 +1,14 @@
+int left = 0;
+
+for(int right = 0; right < arr.length; right++){
+
+    // expand
+
+    while(condition breaks){
+
+        // shrink
+        left++;
+    }
+
+    // update answer
+}

df7fa7afef9df156d26dbfde569de1af7dbb3964
HancithGG <learneasily.2534@gmail.com>
2026-06-26 11:04:32 +0530

    Update Partition Array by Pivot.java

--- 20_80 /arrays/2pointers/partitioning/Partition Array by Pivot.java
+++ 20_80 /arrays/2pointers/partitioning/Partition Array by Pivot.java
@@ -1,24 +1,27 @@
 import java.util.*;
 
-public class Main {
-    public static int[] pivotArray(int[] nums, int pivot) {
-        int[] result = new int[nums.length];
-        int index = 0;
+public static void partitionPivot(int[] arr, int pivot) {
+    int left = 0;
+    int right = arr.length - 1;
 
-        for (int num : nums)
-            if (num < pivot)
-                result[index++] = num;
+    while (left <= right) {
+        while (left <= right && arr[left] < pivot) {
+            left++;
+        }
+        while (left <= right && arr[right] >= pivot) {
+            right--;
+        }
 
-        for (int num : nums)
-            if (num == pivot)
-                result[index++] = num;
-
-        for (int num : nums)
-            if (num > pivot)
-                result[index++] = num;
-
-        return result;
+        if (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;   // move inward
+            right--;  // move inward
+        }
     }
+}
+
 
     public static void main(String[] args) {
         int[] nums = {9,12,5,10,14,3,10};

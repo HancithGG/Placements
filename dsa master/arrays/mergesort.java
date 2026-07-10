432d28553496b6fd7a37dd8ab4ea83e050ed4afa
HancithGG <learneasily.2534@gmail.com>
2026-07-10 18:48:08 +0530

    Create mergesort.java

---
+++ dsa master/arrays/mergesort.java
@@ -0,0 +1,79 @@
+public class MergeSort {
+
+    // Merge two sorted halves
+    static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        // Copy left half
+        for (int i = 0; i < n1; i++)
+            leftArr[i] = arr[left + i];
+
+        // Copy right half
+        for (int j = 0; j < n2; j++)
+            rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        // Merge both arrays
+        while (i < n1 && j < n2) {
+
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        // Copy remaining left elements
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining right elements
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void mergeSort(int[] arr, int left, int right) {
+
+        if (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {7, 2, 9, 1, 5};
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.print("Sorted Array: ");
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}

9c8f5806109e002c061db639ac284871c6ddb797
HancithGG <learneasily.2534@gmail.com>
2026-07-14 20:42:02 +0530

    Update remove dup.java

--- dsa master/arrays/remove dup.java
+++ dsa master/arrays/remove dup.java
@@ -1,1 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0)
+          return 0;
+        int write = 0; // next position to write unique element
+        for (int read = 1; read < nums.length; read++) {
+            if (nums[read] != nums[read - 1]) {
+                nums[write] = nums[read];
+                write++;
+            }
+        }
+        return write; // length of unique array
+    }
+}
 

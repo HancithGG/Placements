182edea4a98098f117e596cd44a052e86b380c42
HancithGG <learneasily.2534@gmail.com>
2026-07-14 20:43:03 +0530

    Create Binary Search.java

---
+++ dsa master/arrays/Binary Search.java
@@ -0,0 +1,40 @@
+public class BinarySearch {
+
+    static int binarySearch(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 5, 8, 10, 15, 18, 20};
+
+        int target = 15;
+
+        int index = binarySearch(arr, target);
+
+        if (index != -1)
+            System.out.println("Element found at index " + index);
+        else
+            System.out.println("Element not found");
+    }
+}

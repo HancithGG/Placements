25a6db5071350baaa14d93e3e339a5c075f3887e
HancithGG <learneasily.2534@gmail.com>
2026-08-01 15:44:46 +0530

    Create bfs.java

---
+++ dsa master/graphs/bfs.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class BFSExample {
+
+    public static void bfs(ArrayList<ArrayList<Integer>> graph, int start) {
+
+        boolean[] visited = new boolean[graph.size()];
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+
+            int current = queue.poll();
+
+            System.out.print(current + " ");
+
+            for (int neighbor : graph.get(current)) {
+
+                if (!visited[neighbor]) {
+
+                    visited[neighbor] = true;
+
+                    queue.offer(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int vertices = 5;
+
+        ArrayList<ArrayList<Integer>> graph =
+                new ArrayList<>();
+
+        for (int i = 0; i < vertices; i++) {
+
+            graph.add(new ArrayList<>());
+        }
+
+        // Undirected Graph
+        graph.get(0).add(1);
+        graph.get(1).add(0);
+
+        graph.get(0).add(2);
+        graph.get(2).add(0);
+
+        graph.get(1).add(3);
+        graph.get(3).add(1);
+
+        graph.get(2).add(4);
+        graph.get(4).add(2);
+
+        bfs(graph, 0);
+    }
+}

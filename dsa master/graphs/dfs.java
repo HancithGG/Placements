d773d1706aea1443ed23feffc2b2e4f828ea6699
HancithGG <learneasily.2534@gmail.com>
2026-08-02 18:40:33 +0530

    Create dfs.java

---
+++ dsa master/graphs/dfs.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class DFSExample {
+
+    static void dfs(ArrayList<ArrayList<Integer>> graph,
+                    int current,
+                    boolean[] visited) {
+
+        visited[current] = true;
+
+        System.out.print(current + " ");
+
+        for (int neighbor : graph.get(current)) {
+
+            if (!visited[neighbor]) {
+
+                dfs(graph, neighbor, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int vertices = 5;
+
+        ArrayList<ArrayList<Integer>> graph =
+                new ArrayList<>();
+
+        for (int i = 0; i < vertices; i++) {
+
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(0).add(1);
+        graph.get(1).add(0);
+
+        graph.get(0).add(2);
+        graph.get(2).add(0);
+
+        graph.get(1).add(3);
+        graph.get(3).add(1);
+
+        graph.get(2).add(4);
+        graph.get(4).add(2);
+
+        boolean[] visited =
+                new boolean[vertices];
+
+        dfs(graph, 0, visited);
+    }
+}
+
+
+// iterative
+
+Stack<Integer> stack = new Stack<>();
+boolean[] visited = new boolean[n];
+
+stack.push(start);
+
+while (!stack.isEmpty()) {
+
+    int current = stack.pop();
+
+    if (visited[current]) {
+        continue;
+    }
+
+    visited[current] = true;
+
+    System.out.print(current + " ");
+
+    for (int i = graph.get(current).size() - 1; i >= 0; i--) {
+
+        int neighbor = graph.get(current).get(i);
+
+        if (!visited[neighbor]) {
+            stack.push(neighbor);
+        }
+    }
+}
+
+// in main method call
+dfs(graph,0);

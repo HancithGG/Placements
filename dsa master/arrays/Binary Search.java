086909bd0893e740e75231db0c6afae06b7a906c
HancithGG <learneasily.2534@gmail.com>
2026-08-21 10:52:31 +0530

    Update Binary Search.java

--- dsa master/arrays/Binary Search.java
+++ dsa master/arrays/Binary Search.java
@@ -1,40 +1,28 @@
-public class BinarySearch {
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
-    static int binarySearch(int[] arr, int target) {
+    while low <= high:
+        mid = low + (high - low) // 2
 
-        int low = 0;
-        int high = arr.length - 1;
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
 
-        while (low <= high) {
+    return -1
 
-            int mid = low + (high - low) / 2;
 
-            if (arr[mid] == target) {
-                return mid;
-            }
+if __name__ == "__main__":
+    arr = [2, 5, 8, 10, 15, 18, 20]
+    target = 15
 
-            if (arr[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+    index = binary_search(arr, target)
 
-        }
+    if index != -1:
+        print(f"Element found at index {index}")
+    else:
+        print("Element not found")
 
-        return -1;
-    }
-
-    public static void main(String[] args) {
-
-        int[] arr = {2, 5, 8, 10, 15, 18, 20};
-
-        int target = 15;
-
-        int index = binarySearch(arr, target);
-
-        if (index != -1)
-            System.out.println("Element found at index " + index);
-        else
-            System.out.println("Element not found");
-    }
-}

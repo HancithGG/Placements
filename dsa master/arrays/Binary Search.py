37afa2996987d3c37c1356c8006e7a5fa8f86152
HancithGG <learneasily.2534@gmail.com>
2026-08-21 11:09:24 +0530

    Update Binary Search.py

--- dsa master/arrays/Binary Search.py
+++ dsa master/arrays/Binary Search.py
@@ -3,7 +3,7 @@ def binary_search(arr, target):
     left, right = 0, len(arr) - 1
     
     while left <= right:
-        mid = (left + right) // 2
+        mid = left + (right - left) // 2
         guess = arr[mid]
         
         if guess == target:
